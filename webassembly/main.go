471d21b9b81d31ea004bc110d203c50d6669cd61
ikeyu0806 <ikeyu0806@gmail.com>
2023-08-27 11:11:57 +0900

    implement go getFileSize function

--- webassembly/main.go
+++ webassembly/main.go
@@ -5,8 +5,23 @@ import (
 	"syscall/js"
 )
 
+func getFileSize(this js.Value, args []js.Value) interface{} {
+	if len(args) != 1 {
+		return nil
+	}
+
+	file := args[0]
+	size := file.Get("size").Int()
+
+	fmt.Printf("Uploaded File Size: %d bytes\n", size)
+
+	return nil
+}
+
 func main() {
-	fmt.Println("fmt Println!")
-	js.Global().Get("console").Call("log", "js.Global Call!")
-	select {}
+	c := make(chan struct{}, 0)
+
+	js.Global().Set("getFileSize", js.FuncOf(getFileSize))
+
+	<-c
 }
