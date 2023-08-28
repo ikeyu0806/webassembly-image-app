06623b29370e7921f711e97b4327e690bb030342
ikeyu0806 <ikeyu0806@gmail.com>
2023-08-28 10:39:02 +0900

    implement loadImageData

--- webassembly/main.go
+++ webassembly/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image"
+	_ "image/png"
 	"syscall/js"
 )
+
+func loadImageData(file js.Value) (image.Image, error) {
+	arrayBuffer := file.Get("arrayBuffer")
+	buffer := make([]byte, arrayBuffer.Get("byteLength").Int())
+	js.CopyBytesToGo(buffer, arrayBuffer)
+
+	imageReader := bytes.NewReader(buffer)
+	img, _, err := image.Decode(imageReader)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
 
 func getFileSize(this js.Value, args []js.Value) interface{} {
 	if len(args) != 1 {
@@ -14,6 +31,14 @@ func getFileSize(this js.Value, args []js.Value) interface{} {
 	size := file.Get("size").Int()
 
 	fmt.Printf("Uploaded File Size: %d bytes\n", size)
+
+	img, err := loadImageData(file)
+	if err != nil {
+		fmt.Printf("Error loading image: %v\n", err)
+		return nil
+	}
+
+	fmt.Printf("img result%v", img)
 
 	return nil
 }
