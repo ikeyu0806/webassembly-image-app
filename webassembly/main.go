f9f82fb0f4e98d88968cdeb9fe3a6baaeade984e
ikeyu0806 <ikeyu0806@gmail.com>
2023-09-16 14:19:21 +0900

    use CopyBytesToGo in getFileSize function

--- webassembly/main.go
+++ webassembly/main.go
@@ -1,44 +1,20 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	"fmt"
-	"image"
-	_ "image/png"
+	// "image"
+	// _ "image/png"
 	"syscall/js"
 )
 
-func loadImageData(file js.Value) (image.Image, error) {
-	arrayBuffer := file.Get("arrayBuffer")
-	buffer := make([]byte, arrayBuffer.Get("byteLength").Int())
-	js.CopyBytesToGo(buffer, arrayBuffer)
-
-	imageReader := bytes.NewReader(buffer)
-	img, _, err := image.Decode(imageReader)
-	if err != nil {
-		return nil, err
-	}
-
-	return img, nil
-}
-
 func getFileSize(this js.Value, args []js.Value) interface{} {
-	if len(args) != 1 {
-		return nil
-	}
+	imageData := args[0]
+	uint8Array := js.Global().Get("Uint8Array").New(imageData)
+	imageBytes := make([]byte, uint8Array.Length())
+	js.CopyBytesToGo(imageBytes, uint8Array)
 
-	file := args[0]
-	size := file.Get("size").Int()
-
-	fmt.Printf("Uploaded File Size: %d bytes\n", size)
-
-	img, err := loadImageData(file)
-	if err != nil {
-		fmt.Printf("Error loading image: %v\n", err)
-		return nil
-	}
-
-	fmt.Printf("img result%v", img)
+	fmt.Printf("Uploaded File Size: %d bytes\n", len(imageBytes))
 
 	return nil
 }
