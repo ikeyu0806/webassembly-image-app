398f1ca2bfef02292a278e296a7f1e573fa5179b
ikeyu0806 <ikeyu0806@gmail.com>
2023-09-17 10:28:34 +0900

    rename uploaded image

--- webassembly/main.go
+++ webassembly/main.go
@@ -17,7 +17,7 @@ func getFileSize(this js.Value, args []js.Value) interface{} {
 	return nil
 }
 
-func convertToBlackAndWhite(this js.Value, args []js.Value) interface{} {
+func showUploadedImage(this js.Value, args []js.Value) interface{} {
 	imageData := args[0]
 	uint8Array := js.Global().Get("Uint8Array").New(imageData)
 	imageBytes := make([]byte, uint8Array.Length())
@@ -42,7 +42,7 @@ func main() {
 	c := make(chan struct{}, 0)
 
 	js.Global().Set("getFileSize", js.FuncOf(getFileSize))
-	js.Global().Set("convertToBlackAndWhite", js.FuncOf(convertToBlackAndWhite))
+	js.Global().Set("showUploadedImage", js.FuncOf(showUploadedImage))
 
 	<-c
 }
